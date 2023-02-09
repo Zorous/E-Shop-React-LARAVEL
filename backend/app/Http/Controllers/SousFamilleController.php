5496cdcdb3e84df7f3d5e4fa4b69a21e256062a0
Zorous <oussamabelhadi5@gmail.com>
2023-02-09 22:57:41 +0100

    init

---
+++ backend/app/Http/Controllers/SousFamilleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SousFamille;
+use Illuminate\Http\Request;
+
+class SousFamilleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SousFamille  $sousFamille
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SousFamille $sousFamille)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SousFamille  $sousFamille
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SousFamille $sousFamille)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SousFamille  $sousFamille
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SousFamille $sousFamille)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SousFamille  $sousFamille
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SousFamille $sousFamille)
+    {
+        //
+    }
+}
