45eec67d09d888e63bf6c73baf81e85495aa6890
Zorous <oussamabelhadi5@gmail.com>
2023-02-10 23:40:33 +0100

    setting up the backend API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,9 +8,6 @@ use App\Http\Controllers\FamilleController;
 
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
