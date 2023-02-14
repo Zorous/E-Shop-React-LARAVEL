1bb7289440f8635821855dd3fdde3fc21c63af4d
Zorous <oussamabelhadi5@gmail.com>
2023-02-14 12:45:46 +0100

    Laravel Sanctum Authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,14 +1,12 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\FamilleController;
-use App\Http\Controllers\SousFamilleController;
-
 /*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
@@ -16,15 +14,6 @@ use App\Http\Controllers\SousFamilleController;
 |
 */
 
-Route::get("/articles",[ArticleController::class,"index"]);
-Route::get("/familles",[FamilleController::class,"index"]);
-Route::get("/sousfamilles",[SousFamilleController::class,"index"]);
-
-
-Route::post('/post',[PostController::class,"store"]);
-
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
